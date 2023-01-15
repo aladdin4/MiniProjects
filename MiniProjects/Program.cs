25d9914e55f06f29a1a17e5be7ad13c210bdf3df
A. Aladdin <a.aladdin.developer@gmail.com>
2023-01-16 01:39:27 +0200

    Update Program.cs

--- MiniProjects/Program.cs
+++ MiniProjects/Program.cs
@@ -10,18 +10,55 @@ namespace MiniProjects
     {
         static void Main(string[] args)
         {
+            // Deck test = new Deck();
+            //test.CreateDeck();
+
+            //for (int i = 0; i < test.fullDeck.Count; i++)
+            //{
+            //    Console.WriteLine(test.fullDeck[i]);
+                
+            //}
+            Console.ReadLine();
+
         }
+
     }
 
     public abstract class Deck
     {
+        //organised not shuffled
+        protected List<PlayingCard> fullDeck = new List<PlayingCard>();
+
+        //the fullDeck after shuffling -- the one we draw from
+        protected List<PlayingCard> drawPile = new List<PlayingCard>();
+
+        protected List<PlayingCard> discardPile = new List<PlayingCard>();
         public void CreateDeck()
         {
+            fullDeck.Clear();
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for (int val = 0; val < 13; val++)
+                {
+                    fullDeck.Add(new PlayingCard { Suit = (CardSuit)suit, Value = (CardValue)val });
+                }
+            }
+            
         }
-        public void ShuffleDeck()
+        public virtual void ShuffleDeck()
         {
+            var rnd = new Random();
+            drawPile = fullDeck.OrderBy(card => rnd.Next()).ToList();
         }
-        public abstract List<PlayingCard> DealCard(int numberOFCards);
+        public abstract PlayingCard DealCard(int numberOFCards);
+
+        public PlayingCard RequestCard()
+        {
+            var output = drawPile.Take(1).First();
+            drawPile.Remove(output);
+
+            return output;
+        }
     }
 
     //single card type
@@ -29,6 +66,11 @@ namespace MiniProjects
     {
         public CardSuit Suit { get; set; }
         public CardValue Value { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"{Value} of {Suit}";
+        //}
     }
     public enum CardSuit
     {
