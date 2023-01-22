29661d6d7a210cda98f7b0d63a528e29281fd619
Aladdin <a.aladdin.developer@gmail.com>
2023-01-22 08:49:12 +0200

    Update Program.cs

--- MiniProjects/Program.cs
+++ MiniProjects/Program.cs
@@ -6,28 +6,55 @@ namespace MiniProjects
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            //public CardSuit Suit { get; set; }
-            //public CardValue Value { get; set; }
 
-            PokerDeck pokerCards = new PokerDeck();
-            var cards = pokerCards.DealCards();
-            foreach (var card in cards)
-            {
-                Console.WriteLine($"{card.Value} of {card.Suit}");
-            }
+            /// <summary>
+            /// The Card Game Init.
+            /// </summary>
+            //PokerDeck pokerCards = new PokerDeck();
+            //var cards = pokerCards.DealCards();
+            //foreach (var card in cards)
+            //{
+            //    Console.WriteLine($"{card.Value} of {card.Suit}");
+            //}
+            //Console.WriteLine("----------------------------------------------------------");
+            //BlackJackDeck blackJackCards = new BlackJackDeck();
+            //var otherCards = blackJackCards.DealCards();
+            //foreach (var card in otherCards)
+            //{
+            //    Console.WriteLine($"{card.Value} of {card.Suit}");
+            //}
 
-            Console.WriteLine("----------------------------------------------------------");
 
-            BlackJackDeck blackJackCards = new BlackJackDeck();
-            var otherCards = blackJackCards.DealCards();
-            foreach (var card in otherCards)
-            {
-                Console.WriteLine($"{card.Value} of {card.Suit}");
-            }
+            Person tim = new Person("Tim", "Corey");
+            tim.PrintPerson();
+            Console.WriteLine($"outside person name is: {tim.FirstName} {tim.LastName}");
 
             Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+    public static class Extensions
+    {
+        public static void PrintPerson(this Person person)
+        {
+            person.FirstName = "changed";
+            person.LastName = "changed";
+            Console.WriteLine($"current person name is: {person.FirstName} {person.LastName}");
+
         }
     }
 
